459d22acc22d83960dcaf8b3c0ff34d25aa183b8
Bigu-16 <137522766+Bigu-16@users.noreply.github.com>
2023-12-07 00:02:04 +0300

    Added solution for Verifying an Alien Dictionary

---
+++ week1/verifying-an-alien-dictionary.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_idx = {c:i for i,c in enumerate(order)}
+
+        for i in range(len(words)-1):
+            word1, word2 = words[i], words[i+1]
+
+            for j in range(len(word1)):
+                if j == len(word2):
+                    return False
+                if word1[j] != word2[j]:
+                    if order_idx[word2[j]] < order_idx[word1[j]]:
+                        return False
+                    break
+        return True
