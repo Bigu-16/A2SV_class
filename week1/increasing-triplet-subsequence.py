73603d26738db44185f830d30535229ae70fa552
Bigu-16 <137522766+Bigu-16@users.noreply.github.com>
2023-12-24 22:26:19 +0300

    Added solution for Increasing Triplet Subsequence

---
+++ week1/increasing-triplet-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        smallest = float('inf')
+        second_smallest = float('inf')
+
+        for num in nums:
+            if num <= smallest:
+                smallest = num
+            elif num <= second_smallest:
+                second_smallest = num
+            else:
+                return True
+        return False
