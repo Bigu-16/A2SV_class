c65930978d8809add9180ad7812eabdb4345724e
Bigu-16 <137522766+Bigu-16@users.noreply.github.com>
2024-01-03 20:03:16 +0300

    Added solution for Count Pairs Whose Sum is Less than Target

---
+++ week1/count-pairs-whose-sum-is-less-than-target.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        count = 0
+
+        nums.sort()
+        while left < right:
+            if nums[left] + nums[right] >= target:
+                right -= 1
+            else:
+                count += right - left
+                left += 1
+        return count
+
